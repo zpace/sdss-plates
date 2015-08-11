429dc29a5d6f8ce3655f62343b7794467e290d79
Zach Pace <zpace21@gmail.com>
2015-08-11 14:35:05 -0500

    fixes for medidian cross issue, plus one new wrapper command and sql search functionality

--- plate_img.py
+++ plate_img.py
@@ -8,6 +8,22 @@ import mpld3
 from mpld3 import plugins, utils
 import pandas as pd
 import matplotlib as mpl
+from os import system, makedirs
+
+
+def get_plate_coords(plate_num):
+    '''
+    use plateX file to find center RA and Dec of a particular plate
+    '''
+    plateX = table.Table.read('plates-dr12.fits', format='fits')
+    row = plateX[np.where(plateX['PLATE'] == int(plate_num))]
+    if len(row) != 131:
+        row = row[0]
+    ra = row['RACEN']
+    dec = row['DECCEN']
+    survey = row['SURVEY'].replace(' ', '')
+    print 'Plate {0} ({1}) @ {2}, {3}'.format(plate_num, survey, ra, dec)
+    return ra, dec, survey
 
 
 def retrieve_ugr(ra, dec, plate_num, width=1., scale=1.):
@@ -62,8 +78,6 @@ def make_SDSS_field_cutout(ra, dec, plate_num, width=3.1, scale=5.5):
     width argument is *in degrees*, and download_sloan_im takes width and height in pixels (with scale in arcsec)
 
     NOTE: there seems to be an artificial limit of 2400*2400 pix. Current defaults seem to play nice, but be careful!
-
-    NOTE: this will likely break along the RA=0h meridian! May hack a fix, or transition to basemap at a later date!
     '''
 
     plt.close('all')
@@ -72,6 +86,13 @@ def make_SDSS_field_cutout(ra, dec, plate_num, width=3.1, scale=5.5):
     print img.shape, (width*3600.)//scale
     ralims = [sum(i) for i in zip([ra, ] * 2, [width/2., -width/2.])]
     declims = [sum(i) for i in zip([dec, ] * 2, [-width/2., width/2.])]
+
+    if (ralims[0] > 360.) & (ralims[1] < 360.):
+        transform = True
+        ralims = [i - 360. for i in ralims]
+        ra -= 360.
+    else:
+        transform = False
 
     fig = plt.figure(figsize=(8.5, 8.), dpi=100)
     ax = fig.add_subplot(111)
@@ -86,6 +107,10 @@ def make_SDSS_field_cutout(ra, dec, plate_num, width=3.1, scale=5.5):
     # read in locations of objects on plate
     plate_objs = table.Table.read(str(plate_num) + '.csv', format='csv',
                                   comment='\#')
+
+    if transform == True:
+        plate_objs['ra'][plate_objs['ra'] > 180.] -= 360.
+
     stars = plate_objs[plate_objs['class'] == 'STAR']
     QSOs = plate_objs[plate_objs['class'] == 'QSO']
     galaxies = plate_objs[plate_objs['class'] == 'GALAXY']
@@ -101,8 +126,6 @@ def make_SDSS_field_cutout(ra, dec, plate_num, width=3.1, scale=5.5):
                marker='v', facecolor='none', edgecolor='r',
                label='Galaxies')
     plt.legend(loc='best')
-
-    #ax.legend(loc='upper right')
 
     # make everything look nice
     ax.set_xlabel('RA [deg]', size=20)
@@ -168,8 +191,6 @@ def make_SDSS_field_mpld3(ra, dec, plate_num, width=3.1, scale=5.5):
     width argument is *in degrees*, and download_sloan_im takes width and height in pixels (with scale in arcsec)
 
     NOTE: there seems to be an artificial limit of 2400*2400 pix. Current defaults seem to play nice, but be careful!
-
-    NOTE: this will likely break along the RA=0h meridian! May hack a fix, or transition to basemap at a later date!
     '''
 
     # Define some CSS to control our custom labels
@@ -202,6 +223,13 @@ def make_SDSS_field_mpld3(ra, dec, plate_num, width=3.1, scale=5.5):
     ralims = [sum(i) for i in zip([ra, ] * 2, [width/2., -width/2.])]
     declims = [sum(i) for i in zip([dec, ] * 2, [-width/2., width/2.])]
 
+    if (ralims[0] > 360.) & (ralims[1] < 360.):
+        transform = True
+        ralims = [i - 360. for i in ralims]
+        ra -= 360.
+    else:
+        transform = False
+
     fig = plt.figure(figsize=(8.5, 8.), dpi=100)
     ax = fig.add_subplot(111)
 
@@ -215,6 +243,10 @@ def make_SDSS_field_mpld3(ra, dec, plate_num, width=3.1, scale=5.5):
     # read in locations of objects on plate
     plate_objs = table.Table.read(str(plate_num) + '.csv', format='csv',
                                   comment='\#')
+
+    if transform == True:
+        plate_objs['ra'][plate_objs['ra'] > 180.] -= 360.
+
     plate_objs = plate_objs[plate_objs['targetObjID'] != 0]
     stars = plate_objs[plate_objs['class'] == 'STAR']
     QSOs = plate_objs[plate_objs['class'] == 'QSO']
@@ -319,3 +351,38 @@ def make_SDSS_field_mpld3(ra, dec, plate_num, width=3.1, scale=5.5):
     plugins.connect(fig, ClickInfo(galaxies_points, list(galaxies['urls'])))
 
     mpld3.save_html(fig, str(plate_num) + '.html')
+
+
+def plate_img_wrapper(plate_num):
+    '''do all the things'''
+
+    # find the center of the plate first
+    ractr, decctr, survey = get_plate_coords(plate_num)
+
+    # now download the list of objects on that plate
+    sql = '\'SELECT sp.specObjID, sp.plate, sp.mjd, sp.ra, sp.dec, sp.z, sp.targetObjID, class FROM SpecObjAll sp WHERE (sp.plate = {}) AND ((class = "GALAXY") OR (class = "QSO") OR (class = "STAR")) AND (sp.targetObjID != 0)\''.format(
+        plate_num)
+
+    # print sql
+
+    # now query SkyServer and throw everything in a csv file
+    try:
+        system('rm {}.csv'.format(plate_num))
+    except:
+        pass
+    system('python sqlcl.py -q ' + sql + ' >> {}.csv'.format(plate_num))
+
+    make_SDSS_field_cutout(ra=ractr, dec=decctr, plate_num=plate_num,
+                           width=3.1, scale=5.5)
+    make_SDSS_field_mpld3(ra=ractr, dec=decctr, plate_num=plate_num,
+                          width=3.1, scale=5.5)
+
+    import shutil
+    shutil.rmtree('{}/'.format(plate_num), ignore_errors=True)
+    makedirs('{}'.format(plate_num))
+    shutil.move('{}.csv'.format(plate_num),
+                '{}/{}_objs.csv'.format(plate_num, plate_num))
+    shutil.move('{}.png'.format(plate_num),
+                '{}/{}_field.png'.format(plate_num, plate_num))
+    shutil.move('{}.html'.format(plate_num),
+                '{}/{}_field_interactive.html'.format(plate_num, plate_num))
